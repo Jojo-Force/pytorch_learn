c72d812f40c669c355ad7b08a9be3a1857470b34
jojo <jojo_zeng@126.com>
2026-02-03 13:46:11 +0800

    update

--- test/神经网络实战分类与回归任务/train.py
+++ test/神经网络实战分类与回归任务/train.py
@@ -11,5 +11,52 @@ warnings.filterwarnings("ignore")
 #%matplotlib inline
 features = pd.read_csv('temps.csv')
 
-a=features
+a=features.head()
 print(a)
+
+import datetime
+years = features['year']
+months = features['month']
+days = features['day']
+
+# datetime格式
+dates = [str(int(year)) + '-' + str(int(month)) + '-' + str(int(day)) for year, month, day in zip(years, months, days)]
+dates = [datetime.datetime.strptime(date, '%Y-%m-%d') for date in dates]
+
+print(dates[:5])
+
+plt.style.use('fivethirtyeight')
+
+# 设置布局
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize = (10,10))
+fig.autofmt_xdate(rotation = 45)
+
+# 标签值
+ax1.plot(dates, features['actual'])
+ax1.set_xlabel(''); ax1.set_ylabel('Temperature'); ax1.set_title('Max Temp')
+
+# 昨天
+ax2.plot(dates, features['temp_1'])
+ax2.set_xlabel(''); ax2.set_ylabel('Temperature'); ax2.set_title('Previous Max Temp')
+
+# 前天
+ax3.plot(dates, features['temp_2'])
+ax3.set_xlabel('Date'); ax3.set_ylabel('Temperature'); ax3.set_title('Two Days Prior Max Temp')
+
+# 我的逗逼朋友
+ax4.plot(dates, features['friend'])
+ax4.set_xlabel('Date'); ax4.set_ylabel('Temperature'); ax4.set_title('Friend Estimate')
+
+plt.tight_layout(pad=2)
+
+features = pd.get_dummies(features)
+print(features.head(5))
+
+labels = np.array(features['actual'])
+
+features = features.drop('actual', axis = 1)
+
+feature_list = list(features.columns)
+features = np.array(features)
+
+print(features.shape)
